fb81678c0b566cbaa6606d5715eb83c732731eb9
PatrickZablocki <Patrick Zablocki>
2024-07-12 18:30:00 +0200

    Ich mach morgen weiter ich hase Responsive Design

---
+++ JS/General.js
@@ -0,0 +1,5 @@
+// Code for Responsive 
+document.getElementById('menuBtn').addEventListener('click', function() {
+    const navbarContent = document.querySelector('.Navbar_content');
+    navbarContent.classList.toggle('show-menu');
+});
